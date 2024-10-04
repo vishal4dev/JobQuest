df64a4f611a86375022b763374436dc729f52d0a
vishal4dev <vishalks112@gmail.com>
2024-10-05 02:09:19 +0530

    company controller and routes created

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './utils/db.js';
 import userRoute from './routes/user.route.js';
+import companyRoute from './routes/company.route.js'
 
 
 dotenv.config({});
@@ -37,6 +38,7 @@ const PORT = process.env.PORT || 3000;
 
 //api routes
 app.use('/api/v1/user',userRoute);
+app.use('/api/v1/company',companyRoute);
 
 
 
