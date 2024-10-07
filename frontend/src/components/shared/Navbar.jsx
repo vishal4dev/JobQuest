78f5c11b4a7fde477c17573d5bea26bb70d91aa5
vishal4dev <vishalks112@gmail.com>
2024-10-08 00:33:31 +0530

    frontend initialized

---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Avatar, AvatarImage } from "../ui/avatar";
+
+const Navbar = () => {
+  return (
+    <div className="bg-white">
+      <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Job<span className="text-[#F83002]">Portal</span>
+          </h1>
+        </div>
+        <div className="flex items-center gap-12">
+          <ul className="flex font-medium items-center gap-5">
+            <li>Home</li>
+            <li>Jobs</li>
+            <li>Browse</li>
+          </ul>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Avatar className="cursor-pointer">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+              </Avatar>
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
+              <div className="flex gap-1">
+                <Avatar className="cursor-pointer">
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="@shadcn"
+                  />
+                </Avatar>
+                <h4 className="font-medium">Vishal Backend Engineer</h4>
+                <p>im here to kick some asses</p>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
